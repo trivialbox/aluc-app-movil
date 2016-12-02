8bd7bb5a4d235b44e9d209d7409419dab2f61d1a
krysthyan <krysthyan_09H@yahoo.com>
2016-12-02 03:12:38 -0500

    peticion post validado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
+
 import { Component } from '@angular/core';
 import { BarcodeScanner } from 'ionic-native'
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { ReservaSrv } from '../../providers/reserva-srv';
+import {Response} from "@angular/http";
 
 @Component({
   selector: 'page-home',
@@ -12,17 +14,41 @@ export class HomePage {
   public mac;
   public token;
   constructor(private navCtrl: NavController,
-              private reservaSrv: ReservaSrv) {
+              private reservaSrv: ReservaSrv,
+              private alerta: AlertController) {
   }
 
   getScanner(){
     BarcodeScanner.scan().then(
         (scanner) => {
-
+          this.reservaSrv.validarReserva(
+              this.mac,
+              this.token,
+              scanner.toString()
+          ).map(
+              (response: Response) => {
+                  this.alertaAluc(response.json().description);
+              }
+          )
         },
         (err) => {
 
+          let alerta_err = this.alerta.create({
+            title: "Error",
+            subTitle: "No se ha podido leer el qr",
+            buttons: ['OK']
+          });
+          alerta_err.present();
         });
+  }
+
+  private alertaAluc(description){
+    let alerta_exito = this.alerta.create({
+      title: "Aluc",
+      subTitle: description,
+      buttons: ['OK']
+    });
+    alerta_exito.present();
   }
 
 }
