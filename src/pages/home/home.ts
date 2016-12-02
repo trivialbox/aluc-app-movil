0f358e674975f8340ab30ed7d4adf0dee45f5579
krysthyan <krysthyan_09H@yahoo.com>
2016-12-02 02:51:32 -0500

    agregado provider e inicalizado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-
+import { BarcodeScanner } from 'ionic-native'
 import { NavController } from 'ionic-angular';
+import { ReservaSrv } from '../../providers/reserva-srv';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [ReservaSrv]
 })
 export class HomePage {
+  public mac;
+  public token;
+  constructor(private navCtrl: NavController,
+              private reservaSrv: ReservaSrv) {
+  }
 
-  constructor(public navCtrl: NavController) {
-    
+  getScanner(){
+    BarcodeScanner.scan().then(
+        (scanner) => {
+
+        },
+        (err) => {
+
+        });
   }
 
 }
