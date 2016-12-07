3dfa07769ca9a45009aca6d8e5f7ec355ace3ae8
krysthyan <krysthyan_09H@yahoo.com>
2016-12-06 23:54:06 -0500

    cambio de text

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,58 +1,70 @@
-
-import { Component } from '@angular/core';
-import { BarcodeScanner } from 'ionic-native'
-import { NavController, AlertController } from 'ionic-angular';
-import { ReservaSrv } from '../../providers/reserva-srv';
-import {Response} from "@angular/http";
-import {_catch} from "rxjs/operator/catch";
+import {Component, Input} from "@angular/core";
+import {BarcodeScanner} from "ionic-native";
+import {Nav, AlertController, NavParams} from "ionic-angular";
+import {ReservaSrv} from "../../providers/reserva-srv";
+import { MenuPage } from '../menu/menu';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
-  providers: [ReservaSrv]
+    selector: 'page-home',
+    templateUrl: 'home.html',
+    providers: [ReservaSrv],
 })
 export class HomePage {
-  public mac = '00:10:49:00:01:02';
-  public token = 'aSDkERTYFRsffge';
-  constructor(private navCtrl: NavController,
-              private reservaSrv: ReservaSrv,
-              private alerta: AlertController) {
-  }
+    public mac = '00:10:49:00:01:02';
+    public token = 'aSDkERTYFRsffge';
+    public cedula;
+    constructor(private nav: Nav,
+                private reservaSrv: ReservaSrv,
+                private alerta: AlertController,
+                private parametros : NavParams) {
+            this.cedula = MenuPage.cedula;
 
-  getScanner(){
-    BarcodeScanner.scan().then(
-        (scanner) => {
-          this.reservaSrv.validarReserva(
-              this.mac,
-              this.token,
-              scanner.text
-          ).subscribe(
-              (response) => {
-                  this.alertaAluc(response.json().description);
-              },
-              (err) => {
-                  this.alertaAluc(err.toString());
-              }
-          )
-        },
-        (err) => {
+    }
 
-          let alerta_err = this.alerta.create({
-            title: "Error",
-            subTitle: "No se ha podido leer el qr",
+    getScanner() {
+        BarcodeScanner.scan().then(
+            (scanner) => {
+                if (scanner.text.length <= 0) {
+
+                } else if (scanner.text.length <= 10 && scanner.text.length >= 1) {
+                    this.alertaAluc('El Qr leido no es valido');
+                } else {
+                    this.validarQr(scanner.text);
+                }
+            },
+            (err) => {
+
+                let alerta_err = this.alerta.create({
+                    title: "Error",
+                    subTitle: "No se ha podido leer el qr",
+                    buttons: ['OK']
+                });
+                alerta_err.present();
+            });
+    }
+
+    private alertaAluc(description) {
+        let alerta_aluc = this.alerta.create({
+            title: "Aluc",
+            subTitle: description,
             buttons: ['OK']
-          });
-          alerta_err.present();
         });
-  }
+        alerta_aluc.present();
+    }
 
-  private alertaAluc(description){
-    let alerta_aluc = this.alerta.create({
-      title: "Aluc",
-      subTitle: description,
-      buttons: ['OK']
-    });
-    alerta_aluc.present();
-  }
+    private validarQr(code_secret) {
+        this.reservaSrv.validarReserva(
+            this.mac,
+            this.token,
+            code_secret
+        ).subscribe(
+            (response) => {
+                this.alertaAluc(response.json().description);
+            },
+            (err) => {
+                this.alertaAluc('Error en la conección al servidor');
+            }
+        )
+    }
 
 }
