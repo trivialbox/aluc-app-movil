6032d4df2b64e1b7af939bdac08392331b2755ec
krysthyan <krysthyan_09H@yahoo.com>
2016-12-06 23:49:40 -0500

    arregado problema de pasar datos

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,34 +1,35 @@
-import { Component, ViewChild } from '@angular/core';
-import {  Nav, Platform, NavParams, NavController } from 'ionic-angular';
-
-import { HomePage } from '../home/home';
-import { LoginPage } from '../login/login';
+import {Component, ViewChild, Input} from "@angular/core";
+import {Nav, Platform, NavParams, NavController} from "ionic-angular";
+import {HomePage} from "../home/home";
+import {LoginPage} from "../login/login";
 
 
 @Component({
-  selector: 'page-menu',
-  templateUrl: 'menu.html'
+    selector: 'page-menu',
+    templateUrl: 'menu.html'
 })
 export class MenuPage {
-  public cedula;
+    static cedula;
+    public num_cedula;
 
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+    rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any, data: JSON}>;
+    pages: Array<{title: string, component: any, icon:string}>;
 
-  constructor(public platform: Platform,
-              private parametro: NavParams,
-              private navCtr: NavController) {
+    constructor(public platform: Platform,
+                private parametro: NavParams,
+                private navCtr: NavController) {
 
-    this.cedula = parametro.get('cedula');
+        MenuPage.cedula = this.parametro.get('cedula');
+        this.num_cedula = MenuPage.cedula;
 
-    this.pages = [
-      { title: 'Validar QR', component: HomePage, data:this.cedula},
-      { title: 'Salir', component: LoginPage, data: undefined}
-    ];
-  }
+        this.pages = [
+            {title: 'Validar QR', component: HomePage, icon:"camera"},
+            {title: 'Salir', component: LoginPage, icon:"close-circle"}
+        ];
+    }
 
   openPage(page) {
     if (page.title == 'Salir'){
@@ -36,7 +37,7 @@ export class MenuPage {
     }else{
       this.nav.setRoot(page.component);
 
+        }
     }
-  }
 
 }
